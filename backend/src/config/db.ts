6a0da9044e9fe914a1eace11143c6522b9cbe350
Ashwin <ap9320000@gmail.com>
2026-03-02 13:03:02 +0530

    db erro solve

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   try {
     const isProduction = process.env.NODE_ENV === "production";
-    const mongoURI =
-      (isProduction ? process.env.MONGO_URI : process.env.LOCAL_MONGO_URI) ||
-      process.env.MONGO_URI;
+    const mongoURI = process.env.MONGO_URI || process.env.LOCAL_MONGO_URI;
 
     if (!mongoURI) {
       throw new Error("MongoDB URI is not defined");
